da97090f2856ee8f70d09bcab93261aae5f0e318
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-07 17:45:31 +0300

    начат encode

--- ProjectSC2000/src/main.rs
+++ ProjectSC2000/src/main.rs
@@ -1,12 +1,14 @@
 mod sc2000;
 
-//use sc2000::*;
+use sc2000::encode;
 use ini::Ini;
 use std::process::abort;
 
 fn main() {
     let mut key = String::new();
     let file = Ini::load_from_file("input.ini").unwrap();
+    let mut input_name = String::new();
+    let mut mode = String::new();
     for (sec, prop) in file.iter() {
         for (k, v) in prop.iter() {
             match (sec, k) {
@@ -16,11 +18,20 @@ fn main() {
                         println!("Key length must be 128!");
                         abort();
                     }
-                },
+                }
+                (Some("Stream"), "File") => input_name = String::from(v),
+                (Some("Stream"), "Mode") => {
+                    mode = String::from(v);
+                    if mode.as_str() != "encoding" && mode.as_str() != "decoding" {
+                        println!("Mode must be \"encoding\" or \"decoding\"!");
+                        abort();
+                    }
+                }
                 _ => {}
             };
         }
     }
     let mut key = u128::from_str_radix(&key, 16).unwrap();
-    println!("{:#018x}", key);
+    println!("Encoding");
+    encode(input_name, key);
 }
