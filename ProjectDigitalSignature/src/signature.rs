d884f88e550cfb87e7117f5c20203016d363b601
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-09 19:09:22 +0300

    Commit 2

--- ProjectDigitalSignature/src/signature.rs
+++ ProjectDigitalSignature/src/signature.rs
@@ -12,7 +12,7 @@ pub fn signature(h: &BigInt) -> (BigInt, BigInt, BigInt) {
     let q = BigInt::parse_bytes(b"252991020016994668398330411224101", 10).unwrap();
     let g = modpow(&BigInt::from(2), &((&p - 1) / &q), &p);
     let mut rng = rand::thread_rng();
-    let k = rng.gen_bigint_range(&BigInt::from(2), &(&q - 1));
+    let k = rng.gen_bigint_range(&BigInt::from(1), &(&q - 1));
     let x = rng.gen_bigint_range(&BigInt::from(2), &(&q - 1));
     let y = modpow(&g, &x, &p);
     let r = modpow(&g, &k, &p);
@@ -26,8 +26,11 @@ pub fn check_signature(h: &BigInt, r: &BigInt, s: &BigInt, y: &BigInt) -> bool {
     let q = BigInt::parse_bytes(b"252991020016994668398330411224101", 10).unwrap();
     let g = modpow(&BigInt::from(2), &((&p - 1) / &q), &p);
     let rho = modpow(r, &BigInt::from(1), &q);
-    let x1 = modpow(r, s, &p);
-    let x2 = modpow(&(&modpow(&modpow(&g, &rho, &p), h, &p) * y), &BigInt::from(1), &p);
+    let x1 = modpow(r, s, &q);
+    let x2 = modpow(
+        &(&modpow(&g, &(&rho * h), &q)
+            * &modpow(y, &BigInt::from(1), &q)),
+        &BigInt::from(1), &q);
     println!("x1 = {}", x1);
     println!("x2 = {}", x2);
     x1 == x2
